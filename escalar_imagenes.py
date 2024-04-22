16cfc89bd9794b2f07191b63c4491dad4c10cbab
Andrea585976 <andreaandres@comunidad.unam.mx>
2024-04-21 23:21:11 -0600

    Modifique escalamiento

--- escalar_imagenes.py
+++ escalar_imagenes.py
@@ -14,11 +14,16 @@ for i in dir_list:   # Para cada imagen en la lista
     im = Image.open(path + name_image)  # Abre la imagen 
     
     width, height = im.size 
-
+    
     # Resize con una proporción 1/3:1/3, es decir, hace la imagen 1/3 mas pequeña en largo y ancho
-    new_size = (round(width/3), round(height/3)) 
+    # new_size = (round(width/3), round(height/3)) 
+    # im1 = im.resize(new_size)
+    
+    # Resize con ancho 600 px y altura variable de tal forma que conserven su misma relación de aspecto
+    new_height = round((height/width)*600)
+    new_size = (600, new_height)
     im1 = im.resize(new_size)
     
     # Guardar imagen
     # Color ruta en donde se guardarán las imágenes escaladas 
-    im1.save(r"A:\Descargas\wikiart\rescaled_max_size_to_600px_same_aspect_ratio" + name_image)
+    im1.save(r"A:\ArtEmis-ProyectoPLN\Imagenes_escaladas" + name_image)
